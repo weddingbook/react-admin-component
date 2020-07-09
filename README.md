74754a6577867aa4d922ba507469f9a2c1a5bc5e
keunhyeok <info@weddingbook.com>
2020-07-09 18:47:02 +0900

    build

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 ```
 yarn build
 ```
+
+## 사용법 
+프로젝트 package.json 파일의 dependency 추가
+```
+"dependencies": {
+	"react-admin-component": "git+https://bitbucket.h2m.io/scm/in_house/react-admin-component.git"
+}
+```
--- build/index.es.js
+++ build/index.es.js
@@ -5,7 +5,8 @@ var MyComponent = function () {
 };
 
 var MyComponent2 = function () {
-    return (React.createElement("div", null, "sample 2 \uC785\uB2C8\uB2E4\uC694!~~~"));
+    return (React.createElement("div", null,
+        React.createElement("p", null, "\uADF8\uB7F0\uB370 \uC5B4\uB5BB\uAC8C \uD574\uC57C \uC5C5\uB370\uC774\uD2B8\uAC00 \uB418\uB294\uAC78\uAE4C?")));
 };
 
 export { MyComponent, MyComponent2 };
