b69adccbe28794a232f4166086c767485b18bd61
keunhyeok <info@weddingbook.com>
2020-07-09 18:34:35 +0900

    어라.?

--- build/index.es.js
+++ build/index.es.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 var MyComponent = function () {
-    return (React.createElement("div", null, "sample!"));
+    return (React.createElement("div", null, "sample! 1\uC785\uB2C8\uB2E4"));
 };
 
 var MyComponent2 = function () {
-    return (React.createElement("div", null, "sample 2 \uC785\uB2C8\uB2E4\uC694!!!"));
+    return (React.createElement("div", null, "sample 2 \uC785\uB2C8\uB2E4\uC694!~~~"));
 };
 
 export { MyComponent, MyComponent2 };
--- build/index.js
+++ build/index.js
@@ -7,11 +7,11 @@ function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'defau
 var React = _interopDefault(require('react'));
 
 var MyComponent = function () {
-    return (React.createElement("div", null, "sample!"));
+    return (React.createElement("div", null, "sample! 1\uC785\uB2C8\uB2E4"));
 };
 
 var MyComponent2 = function () {
-    return (React.createElement("div", null, "sample 2 \uC785\uB2C8\uB2E4\uC694!!!"));
+    return (React.createElement("div", null, "sample 2 \uC785\uB2C8\uB2E4\uC694!~~~"));
 };
 
 exports.MyComponent = MyComponent;
