aa91e92fd652dd04ebe1e83a5b3efde5913b75a3
weddingbook_mw <minwon.ahn@weddingbook.com>
2020-07-16 12:02:50 +0900

    공통 어드민 컴포넌트로 스토리북 동작하도록 수정

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,24 @@
+const path = require("path");
+
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
-  addons: [
-    '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-  ],
+  stories: ["../src/stories/**/*.stories.tsx"],
+  addons: ['@storybook/preset-scss', '@storybook/addon-actions', '@storybook/addon-links', '@storybook/addon-knobs/register'],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["sass-loader"],
+      include: path.resolve(__dirname, "../"),
+    });
+
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve("babel-loader"),
+      options: {
+        presets: [["react-app", { flow: false, typescript: true }]],
+      },
+    });
+    config.resolve.extensions.push(".ts", ".tsx", "js");
+
+    return config;
+  },
 };
