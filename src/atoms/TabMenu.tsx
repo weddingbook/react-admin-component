a4503933cef2cb69783ef5798bc575fe66245e6f
weddingbook_mw <minwon.ahn@weddingbook.com>
2020-08-04 13:49:11 +0900

    [PROD-4259] 탭 컴포넌트 개발 (페이지 탭은 수정 해야함.)

--- src/atoms/TabMenu.tsx
+++ src/atoms/TabMenu.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
 import './TabMenu.scss';
+import ButtonTypeInput from './ButtonTypeInput';
 
 type Props = {
     tabList: string[]
     onSelectMenu: (tabMenu: string) => void;
     selectedMenu: string
+    type?: 'page' | 'modal' | 'section' | 'list'
 }
 
-const TabMenu = ({ tabList, onSelectMenu, selectedMenu }: Props) => {
+export const PageTabMenu = ({ tabList }: Props) => {
+    return <ul className='page-tab-menu'>
+        {tabList.map(value => {
+            return (<li key={value}>{value}</li>)
+        })}
+    </ul>
+}
+
+
+const TabMenu = ({ tabList, onSelectMenu, selectedMenu, type = 'modal' }: Props) => {
     return (
         <>
-            <nav className='tab-menu'>
+            <nav className={`tab-menu ${type === 'list' ? 'list-tab' : ''}`}>
                 <ul>
-                    {tabList.map((value: string) => (<li key={value} onClick={() => { onSelectMenu(value) }} className={`${selectedMenu === value ? 'on' : ''}`}>
-                        {value}
-                    </li>))}
+                    {tabList.map((value: string) =>
+                        <li 
+                            key={value} 
+                            onClick={() => { onSelectMenu(value) }} 
+                            className={`${type === 'list' ? 'list-tab-item' : 'modal-tab-item'} ${selectedMenu === value ? 'on' : ''}`}
+                        >
+                            {value}
+                        </li>
+                    )}
                 </ul>
             </nav>
-            <div className='divider in-tab-menu'></div>
+            {type === ('modal' || 'section') && <div className='divider in-tab-menu'></div>}
         </>
     )
 }
