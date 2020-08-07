632ced3c0afe5536a45e11d27c8488947a4791e0
weddingbook_mw <minwon.ahn@weddingbook.com>
2020-08-07 15:50:45 +0900

    [PROD-4363] build

--- dist/atoms/InputRecommend.d.ts
+++ dist/atoms/InputRecommend.d.ts
@@ -1,11 +1,12 @@
-import { CSSProperties } from 'react';
+import { CSSProperties, ReactComponentElement } from 'react';
 import './InputRecommend.scss';
 declare type Props = {
     options: Array<any>;
     style?: CSSProperties;
+    recommendListButton?: ReactComponentElement<'button'>;
     onSelectOptionSet: (option: any) => void;
     invalid: boolean;
     informationMessage: string;
 };
-declare const InputRecommend: ({ options, style, onSelectOptionSet, invalid, informationMessage }: Props) => JSX.Element;
+declare const InputRecommend: ({ options, style, recommendListButton, onSelectOptionSet, invalid, informationMessage }: Props) => JSX.Element;
 export default InputRecommend;
--- dist/index.js
+++ dist/index.js
@@ -2090,7 +2090,7 @@ var DefinitionTag = function (_a) {
 };
 
 var InputRecommend = function (_a) {
-    var options = _a.options, style = _a.style, onSelectOptionSet = _a.onSelectOptionSet, invalid = _a.invalid, informationMessage = _a.informationMessage;
+    var options = _a.options, style = _a.style, recommendListButton = _a.recommendListButton, onSelectOptionSet = _a.onSelectOptionSet, invalid = _a.invalid, informationMessage = _a.informationMessage;
     var _b = React.useState(true), isShowList = _b[0], setShowList = _b[1];
     var _c = React.useState('0'), marginTop = _c[0], setMarginTop = _c[1];
     var selectListRef = React.useRef(null);
@@ -2118,7 +2118,9 @@ var InputRecommend = function (_a) {
     var onClickOption = function (option) {
         onSelectOptionSet(option);
     };
-    return (React__default.createElement(React__default.Fragment, null, isShowList && React__default.createElement("ul", { ref: selectListRef, className: "input-recommend", style: __assign(__assign({}, style), { marginTop: marginTop }) }, options.map(function (option) { return (React__default.createElement("li", { key: option, onClick: function () { onClickOption(option); } }, option)); }))));
+    return (React__default.createElement(React__default.Fragment, null, isShowList && React__default.createElement("ul", { ref: selectListRef, className: "input-recommend", style: __assign(__assign({}, style), { marginTop: marginTop }) },
+        options.map(function (option) { return (React__default.createElement("li", { key: option, onClick: function () { onClickOption(option); } }, option)); }),
+        recommendListButton)));
 };
 
 var SelectBox = function (_a) {
@@ -2229,14 +2231,14 @@ var Pagination = function (_a) {
 };
 
 var Input = function (_a) {
-    var style = _a.style, inputStyle = _a.inputStyle, iconName = _a.iconName, afterString = _a.afterString, recommendOptions = _a.recommendOptions, onSelectRecommendOptionSet = _a.onSelectRecommendOptionSet, _b = _a.errorMessage, errorMessage = _b === void 0 ? 'Error message' : _b, _c = _a.informationMessage, informationMessage = _c === void 0 ? '' : _c, _d = _a.required, required = _d === void 0 ? false : _d, rest = __rest(_a, ["style", "inputStyle", "iconName", "afterString", "recommendOptions", "onSelectRecommendOptionSet", "errorMessage", "informationMessage", "required"]);
+    var style = _a.style, inputStyle = _a.inputStyle, iconName = _a.iconName, afterString = _a.afterString, recommendListButton = _a.recommendListButton, recommendOptions = _a.recommendOptions, onSelectRecommendOptionSet = _a.onSelectRecommendOptionSet, _b = _a.errorMessage, errorMessage = _b === void 0 ? 'Error message' : _b, _c = _a.informationMessage, informationMessage = _c === void 0 ? '' : _c, _d = _a.required, required = _d === void 0 ? false : _d, rest = __rest(_a, ["style", "inputStyle", "iconName", "afterString", "recommendListButton", "recommendOptions", "onSelectRecommendOptionSet", "errorMessage", "informationMessage", "required"]);
     return (React__default.createElement(React__default.Fragment, null,
         React__default.createElement("div", { className: "input-component " + ((required || informationMessage) ? 'message' : ''), style: style },
             React__default.createElement("div", { className: "default-input-wrapper " + (iconName ? 'after-string' : '') + " " + (required ? 'error' : ''), style: inputStyle },
                 iconName && React__default.createElement(Icon, { name: iconName, size: "18" }),
                 React__default.createElement("input", __assign({}, rest, { className: "" + (afterString ? 'after-string' : ''), required: required })),
                 afterString && React__default.createElement("span", { className: 'unit' }, afterString)),
-            (recommendOptions && onSelectRecommendOptionSet) && React__default.createElement(InputRecommend, { invalid: required, informationMessage: informationMessage, options: recommendOptions, onSelectOptionSet: onSelectRecommendOptionSet }),
+            (recommendOptions && onSelectRecommendOptionSet) && React__default.createElement(InputRecommend, { invalid: required, informationMessage: informationMessage, recommendListButton: recommendListButton, options: recommendOptions, onSelectOptionSet: onSelectRecommendOptionSet }),
             informationMessage && React__default.createElement("span", { className: 'input-message' }, informationMessage),
             (required && errorMessage) && React__default.createElement("span", { className: 'input-message error' }, errorMessage))));
 };
