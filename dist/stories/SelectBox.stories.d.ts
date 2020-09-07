c8102c8d72bfa94a18443e2d75b1d795a82776bf
weddingbook_mw <minwon.ahn@weddingbook.com>
2020-09-07 17:47:51 +0900

    build

--- dist/stories/SelectBox.stories.d.ts
+++ dist/stories/SelectBox.stories.d.ts
@@ -2,8 +2,14 @@ import React from 'react';
 declare const _default: {
     title: string;
     component: ({ options, selectedOption, style, onSelectOptionSet }: {
-        options: any[];
-        selectedOption: React.ReactText;
+        options: {
+            value: any;
+            name: string;
+        }[];
+        selectedOption: {
+            value: any;
+            name: string;
+        };
         style?: React.CSSProperties | undefined;
         onSelectOptionSet: (option: any) => void;
     }) => JSX.Element;
