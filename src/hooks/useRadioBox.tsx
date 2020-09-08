7555764c29b6f9756828f9d99745634cad5e25ea
weddingbook_mw <minwon.ahn@weddingbook.com>
2020-09-08 14:08:33 +0900

    useRadioBox에 기본 요소 변경하는 함수 추가함

--- src/hooks/useRadioBox.tsx
+++ src/hooks/useRadioBox.tsx
@@ -3,12 +3,17 @@ import { useCallback, useState } from 'react';
 export default function useRadioBox(defaultList: Array<any>) {
     const [list, setList] = useState(defaultList)
 
+    const onChangeDefault = (list: any[]) => {
+        setList(list);
+    }
+
     const onChange = useCallback((value: any) => {
         setList(list.map((item: any) => ({...item, checked: item.id === value.id ? true : false})))
     }, [list, setList])
 
     return {
         list,
-        onChange
+        onChange,
+        onChangeDefault
     };
 }
