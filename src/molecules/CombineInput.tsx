9fb8e4925c53cb2ae2a80e8016cdb602cf704014
phanhuypy.1999@gmail.com <phanhuypy.1999@gmail.com>
2021-06-01 16:54:05 +0700

    change align

--- src/molecules/CombineInput.tsx
+++ src/molecules/CombineInput.tsx
@@ -12,7 +12,7 @@ const CombineInput = ({ children, style, title }: Props) => {
         <div className='combine-input-container' style={{...style}}>
             <span>{title}</span>
             <div className='combine-input-list' style={{ gridTemplateColumns: `repeat(auto-fill, minmax(${(1 / children.length) * 100}%, 1fr)`}}>
-                {children}
+                {children}.search-box-wrapper
             </div>
         </div>
     )
