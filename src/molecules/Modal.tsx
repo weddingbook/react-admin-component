cb54f43b0ddc9848c82e3006f8afa57e326afdf3
phanhuypy.1999@gmail.com <phanhuypy.1999@gmail.com>
2021-07-02 15:44:56 +0700

    feature: add props for modal

--- src/molecules/Modal.tsx
+++ src/molecules/Modal.tsx
@@ -1,9 +1,12 @@
-import React, {CSSProperties} from 'react';
+import React, { CSSProperties } from 'react';
 import './Modal.scss';
 
 type Props = {
     children?: object,
     style?: CSSProperties,
+    styleHeader?: CSSProperties,
+    styleBody?: CSSProperties,
+    styleFooter?: CSSProperties,
     title?: string,
     subTitle?: string,
     size?: 'small' | 'medium' | 'large',
@@ -11,33 +14,36 @@ type Props = {
     cancelButtonComponent?: object,
     actionButtonComponent?: object,
     footerComponent?: object,
-	withSection?: boolean,
+    withSection?: boolean,
     noScroll?: boolean,
 }
 
 const Modal = ({
-		children, style, title = 'Modal title', subTitle, size = 'small',
-		withSection = false,
-		noScroll = false,
-		onClickInBackground = () => {
-		}, cancelButtonComponent, actionButtonComponent, footerComponent
-	}: Props) => {
+    children, style, title = 'Modal title', subTitle, size = 'small',
+    styleHeader = {},
+    styleBody = {},
+    styleFooter = {},
+    withSection = false,
+    noScroll = false,
+    onClickInBackground = () => {
+    }, cancelButtonComponent, actionButtonComponent, footerComponent
+}: Props) => {
     return (
         <div className='modal-wrap'>
-            <div className='overlay' onClick={onClickInBackground}/>
+            <div className='overlay' onClick={onClickInBackground} />
             <div className={`modal modal-size-${size} ${noScroll && 'no-scroll'}`} style={style}>
-				<header className="modal-header">
-					<div className='header-top'>
-						<h1>{title}</h1>
-						<div className='btn-area'>
-							{cancelButtonComponent}
-							{actionButtonComponent}
-						</div>
-					</div>
-					<div className='header-bottom'>{subTitle}</div>
-				</header>
-				<div className={`modal-contents ${withSection ? 'with-section' : ''}`}>{children}</div>
-				{footerComponent && <footer>{footerComponent}</footer>}
+                <header className="modal-header" style={styleHeader}>
+                    <div className='header-top'>
+                        <h1>{title}</h1>
+                        <div className='btn-area'>
+                            {cancelButtonComponent}
+                            {actionButtonComponent}
+                        </div>
+                    </div>
+                    <div className='header-bottom'>{subTitle}</div>
+                </header>
+                <div style={styleBody} className={`modal-contents ${withSection ? 'with-section' : ''}`}>{children}</div>
+                {footerComponent && <footer style={styleFooter}>{footerComponent}</footer>}
             </div>
         </div>
     )
