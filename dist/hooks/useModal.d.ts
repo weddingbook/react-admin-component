22c5caab31cd82afd008efa2d6e53a941902ee06
phanhuypy.1999@gmail.com <phanhuypy.1999@gmail.com>
2021-08-25 11:01:25 +0700

    update multi

--- dist/hooks/useModal.d.ts
+++
@@ -1,5 +0,0 @@
-export default function useModal(): {
-    modalToggle: boolean;
-    onOpenModal: () => void;
-    onCloseModal: () => void;
-};
--- dist/molecules/DefinitionTagContainer.d.ts
+++
@@ -1,8 +0,0 @@
-import { CSSProperties } from 'react';
-import './DefinitionTagContainer.scss';
-declare type Props = {
-    children: object;
-    style?: CSSProperties;
-};
-declare const DefinitionTagContainer: ({ children, style }: Props) => JSX.Element;
-export default DefinitionTagContainer;
