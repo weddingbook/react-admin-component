2ad2033aa4ad7509ea22d025a08704f7d2a62622
seongonecho <seongonecho@gmail.com>
2022-03-13 20:19:06 +0900

    build

--- dist/molecules/RangePicker.d.ts
+++ dist/molecules/RangePicker.d.ts
@@ -1,5 +1,5 @@
 import './RangePicker.scss';
 import React from 'react';
-declare const AntRangePicker: React.ComponentClass<import("antd/lib/date-picker/generatePicker").RangePickerProps<import("moment").Moment>, any>;
+declare const AntRangePicker: import("antd/lib/date-picker/generatePicker/interface").PickerComponentClass<import("antd/lib/date-picker/generatePicker").RangePickerProps<import("moment").Moment>, unknown>;
 declare const RangePicker: (props: React.ComponentProps<typeof AntRangePicker>) => JSX.Element;
 export default RangePicker;
